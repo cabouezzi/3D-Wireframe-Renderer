0cd6fbc69676a65002afbeb46666c75c5c481da0
cabouezzi <chanielezzi@gmail.com>
2022-11-04 20:09:08 -0400

    Initial Commit

---
+++ 3D/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  3D
+//
+//  Created by Chaniel Ezzi on 11/4/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
